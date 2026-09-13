5df0f884402246a8c69497d6bb996e93ddd867d9
TheLOE <natetv07@gmail.com>
2026-09-13 22:19:05 +0900

    first setup

---
+++ src/main/kotlin/org/sdxmq/manHunt/ManHunt.kt
@@ -0,0 +1,14 @@
+package org.sdxmq.manHunt
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ManHunt : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
